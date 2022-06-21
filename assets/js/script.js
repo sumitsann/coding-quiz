46b63749febd9d8299dad475709a6d0cdc5c1c96
Sumit Ojha <sumit.ojha.dev@gmail.com>
2022-06-21 16:30:39 -0500

    first change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,82 @@
-let time = 75;
-let runningTimer = 0;
-let score = 0;
-let userName = "";
-let questionNumber;
-let finalScore = 0;
-const MAX_HIGH_SCORE = 5;
-const startButton = document.getElementById("startButton");
-const questionContainer = document.getElementById("questionsContainer");
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answers");
-const countdown = document.getElementById("timerArea");
-const scoreArea = document.getElementById("scoreArea");
-const highScoreButton = document.getElementById("showScoresButton");
-let highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+const startBtn = document.querySelector("#start-btn");
+const questionContainerElement = document.querySelector("#question-container");
+const questionElement = document.querySelector("#question");
+const answerButtonsElement = document.querySelector("#answer-buttons");
+let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener("click", startGame);
-highScoreButton.addEventListener("click", displayscores);
+startBtn.addEventListener("click", startGame);
 
-// starting game function
+// function to start the game
 
 function startGame() {
-  startButton.classList.add("hide");
-  scoreArea.classList.add("hide");
-  answerButtons.classList.remove("hide");
-  questionNumber = 0;
-  questionContainer.classList.remove("hide");
-  scoreArea.innerHTML = "";
-  startClock();
-  displayQuestion();
+  startBtn.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  console.log(shuffledQuestions);
+  questionContainerElement.classList.remove("hide");
+  showQuestion(0);
 }
 
-// function to display the questions
+function showQuestion(currentQuestionIndex) {
+  questionElement.innerText = questions[0].question;
+  console.log(questions.question);
+}
 
-function displayQuestion() {}
-
-// displaying score function
-
-function displayscores() {}
+const questions = [
+  {
+    question: "JavaScript is the programming language of the",
+    answers: [
+      { text: "desktop", correct: false },
+      { text: "mobile", correct: false },
+      { text: "web", correct: true },
+      { text: "server", correct: false },
+    ],
+  },
+  {
+    question: " Which type of JavaScript language is",
+    answers: [
+      { text: "Object-oriented", correct: false },
+      { text: "Object-based", correct: true },
+      { text: "Functional programming", correct: false },
+      { text: "All of the above", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which of the following statement(s) is true about the JavaScript?",
+    answers: [
+      { text: "desktop", correct: false },
+      { text: "mobile", correct: false },
+      { text: "web", correct: true },
+      { text: "server", correct: false },
+    ],
+  },
+  {
+    question: "JavaScript is the programming language of the",
+    answers: [
+      {
+        text: "It is a scripting language used to make the website interactive",
+        correct: true,
+      },
+      {
+        text: "It is an advanced version of Java for Desktop and Mobile application development",
+        correct: false,
+      },
+      {
+        text: "It is a markup language of Java to develop the webpages",
+        correct: false,
+      },
+      { text: "All of the above", correct: false },
+    ],
+  },
+  {
+    question:
+      "Which property is used to define the HTML content to an HTML element with a specific id?",
+    answers: [
+      { text: "innerText", correct: false },
+      { text: "innerContent", correct: false },
+      { text: "elementText", correct: false },
+      { text: "innerHTML", correct: true },
+    ],
+  },
+];
